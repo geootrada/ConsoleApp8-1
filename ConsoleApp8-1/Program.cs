80cb2b4efd4e4e94979de0783b332407f207af0e
geootrada <geootrada@yandex.ru>
2024-02-16 17:32:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp8-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold;
+            int crystals;
+            int priceUnitCrystal = 10;
+            int remainingGold;
+
+            Console.WriteLine("Добро пожаловать в наш магазин кристаллов.");
+            Console.Write("Скажите, сколько у вас золота: ");
+            gold = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Сегодня в продаже кристаллы по " + priceUnitCrystal + " золота.");
+            Console.Write("Скажите, сколько кристаллов вы хотите купить: ");
+            crystals = Convert.ToInt32(Console.ReadLine());
+
+            gold = gold - crystals * priceUnitCrystal;
+            Console.WriteLine($"У вас теперь кристаллов {crystals}, остатаок золотв {gold}. Приятно было иметь с вами дело!");
+        }
+    }
+}
